523abab871452b2ed1f46fa5fc2916f5dc14cfd4
Avraham Kaplan <99396617+AviLinko@users.noreply.github.com>
2023-01-18 15:29:27 +0200

    .

--- Task1/quat_rotation.py
+++ Task1/quat_rotation.py
@@ -3,6 +3,8 @@ import numpy as np
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
+
+
 class Sensor:
     def __init__(self, name, translation, rotation):
         self.name = name
@@ -33,7 +35,7 @@ class BothSensors:
 
     def rotate(self, s1_new_rotation):
         r1_t0 = self.sensor1.rotation
-        r1_t1 = R.from_euler('xyz', s1_new_rotation)
+        r1_t1 = R.from_quat(s1_new_rotation)
         r2_t1 = self.sensor2.rotation
 
         diff = euler_angle_difference(r1_t0,r1_t1) 
@@ -54,21 +56,21 @@ def euler_angle_difference(s1_rotation, s1_new_rotation):
      
 
 if __name__ == "__main__":
-    r1 = [0, 0, 0]
-    r2 = [0, 0, 0]
-    s1_rotation_0 = R.from_euler('xyz', r1)
-    s2_rotation_0 = R.from_euler('xyz', r2)
+    r1 = [1, 0, 0, 0]
+    r2 = [1, 0, 0, 0]
+    s1_rotation_0 = R.from_quat(r1)
+    s2_rotation_0 = R.from_quat(r2)
     
     s1_translation_0 = np.array([0,0,0])
-    s2_translation_0 = np.array([0,0,0])
+    s2_translation_0 = np.array([0,0,2])
 
     sensor1 = Sensor("Sensor 1", s1_translation_0, s1_rotation_0)
     sensor2 = Sensor("Sensor 2", s2_translation_0, s2_rotation_0)
     sensors = BothSensors(sensor1, sensor2)
 
     
-    euler_angles = [0, 0, np.pi/2]
-    new_translation_s1 = np.array([0,0,0])
+    euler_angles = [0, 0, 0, np.pi]
+    new_translation_s1 = np.array([0,0,1])
 
     sensors.move(new_translation_s1)
     sensors.rotate(euler_angles)
@@ -76,7 +78,7 @@ if __name__ == "__main__":
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
     
-    rotated_square = [sensor1.rotation.apply(point) for point in sensor2.square]
+    rotated_square = [sensor2.rotation.apply(point) for point in sensor2.square]
     
     x = [point[0] for point in rotated_square]
     y = [point[1] for point in rotated_square]
