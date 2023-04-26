442da1a04f5d85b0e3844047a07870362fe25f27
adi <adi@tevel-tech.com>
2023-04-27 01:13:29 +0300

    a correction to sensors.py only position

--- Sensors/Sensors.py
+++ Sensors/Sensors.py
@@ -2,9 +2,12 @@ import numpy as np
 
 import pandas as pd
 
-left_sensor = r"C:\Users\avka9\Desktop\study\projects\final_project\Sensors\left_sensor.csv"
+import matplotlib.pyplot as plt
+from pyquaternion import Quaternion
 
-right_sensor = r"C:\Users\avka9\Desktop\study\projects\final_project\Sensors\right_sensor.csv"
+left_sensor = r"/home/adi/workspace/student_projects/jce_2023/final_project/Sensors/left_sensor.csv"
+
+right_sensor = r"/home/adi/workspace/student_projects/jce_2023/final_project/Sensors/right_sensor.csv"
 
 
 # read in the sensor data from the csv files
@@ -23,31 +26,52 @@ sensor1_quat = sensor1_data[['pose.orientation.x', 'pose.orientation.y', 'pose.o
 
 sensor2_pos = sensor2_data[['pose.position.x', 'pose.position.y', 'pose.position.z']].values
 
-sensor2_quat = sensor2_data[['pose.orientation.x', 'pose.orientation.y', 'pose.orientation.z', 'pose.orientation.w']].values
+sensor2_quat = sensor2_data[['pose.orientation.w','pose.orientation.x', 'pose.orientation.y', 'pose.orientation.z']].values
 
 # define the calibration matrix between sensor1 and sensor2
 
-calibration_matrix = np.array([[0.0, 0.0, 1.0, 0.0],
-                               [-1.0, 0.0, 0.0, 0.0],
-                               [0.0, -1.0, 0.0, 0.0],
+# calibration_matrix = np.array([[0.0, 0.0, 1.0, 0.0],
+#                                [-1.0, 0.0, 0.0, 0.0],
+#                                [0.0, -1.0, 0.0, 0.0],
+#                                [0.0, 0.0, 0.0, 1.0]])
+calibration_matrix = np.array([[-1.0, 0.0, 0.0, 0.0],
+                               [0.0, 1.0, 0.0, 0.0],
+                               [0.0, 0.0, -1.0, 0.6216],
                                [0.0, 0.0, 0.0, 1.0]])
-
+rotation_matrix = calibration_matrix[:3,:3]
+translation_vector = calibration_matrix[:3,3]
 # calculate the transformation matrix to convert sensor2 data to sensor1 data
-transformation_matrix = np.dot(np.linalg.inv(calibration_matrix), np.array([[1, 0, 0, -sensor1_pos[0][0]],
-                                                                           [0, 1, 0, -sensor1_pos[0][1]],
-                                                                           [0, 0, 1, -sensor1_pos[0][2]],
-                                                                           [0, 0, 0, 1]]))
+# transformation_matrix = np.dot(np.linalg.inv(calibration_matrix), np.array([[1, 0, 0, -sensor1_pos[0][0]],
+#                                                                            [0, 1, 0, -sensor1_pos[0][1]],
+#                                                                            [0, 0, 1, -sensor1_pos[0][2]],
+#                                                                            [0, 0, 0, 1]]))
 
 # extract the rotation matrix from sensor2_quat and multiply it with the transpose of calibration_matrix
 sensor2_rot = sensor2_quat[:, :3]
 
-sensor1_quat = np.dot(sensor2_rot, calibration_matrix[:3, :3].T)
+# sensor1_quat_calculated = np.dot(sensor2_rot, calibration_matrix[:3, :3].T)
 
 
 # print out the position and quaternion of sensors
 
-sensor1_pos = np.dot(transformation_matrix, np.concatenate((sensor2_pos.T, np.ones((1, sensor2_pos.shape[0])))))
-
+# sensor1_pos_calculated = np.dot(calibration_matrix, np.concatenate((sensor2_pos.T, np.ones((1, sensor2_pos.shape[0]))))).T
+r = []
+for q in sensor2_quat:
+    q_quat = Quaternion(q)
+    r.append(q_quat.rotate(translation_vector))
+sensor1_pos_calculated  = sensor1_pos_calculated = np.dot(rotation_matrix,(sensor2_pos+np.array(r)).T).T#+translation_vector
 print("Sensor 1 Position: ", sensor1_pos[:3])
+start_index = 1000
+end_index = 3000
+translation_0  = sensor1_pos[start_index,:]-sensor1_pos_calculated[start_index,:]
+sensor1_pos_calculated   = sensor1_pos_calculated + translation_0
+
+plt.figure()
+for ax in range(3):
+    plt.subplot(3,1,ax+1)
+    plt.plot(sensor1_pos[start_index:end_index,ax],'.-')
+    # plt.plot(sensor2_pos[start_index:end_index,ax],'.-')
+    plt.plot(sensor1_pos_calculated[start_index:end_index,ax],'+')
+plt.show()
 
 print("Sensor 1 Quaternion: ", sensor1_quat)
